4313f010d8eec0eadf9078ad5b703ec55fd03be2
Kevin Bett <prodigy4614@gmail.com>
2025-10-27 18:56:03 +0300

    update homepage with basic html structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,17 @@ function App() {
 
   return (
     <>
-      <p>
+      <h1>
         Welcome to CodeBase Genius
+      </h1>
+      <p>
+        Codebase Genius is an agentic platform running on JAC programming language.
       </p>
+      <p>
+        Using a collection of agents the platform can orchestarate a repository mapping to analyze the code contained within a public Github repository. You simply provide a link for the public github repository and the agents get to work to generate a comprehensive documentation of the codebase.
+      </p>
+      <label htmlFor="repoUrl">Repository Url</label>
+      <input type="text" name="repoUrl" id="repoUrl" />
     </>
   )
 }
